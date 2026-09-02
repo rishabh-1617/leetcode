d7ab19d36a7c0a1af05711fa6a7db71b43b36474
Rishabh Prajapati <142728606+rishabh-1617@users.noreply.github.com>
2026-09-02 12:33:08 +0530

    Time: 0 ms (100%), Space: 19.2 MB (84.34%) - LeetHub

---
+++ 4256-construct-uniform-parity-array-i/4256-construct-uniform-parity-array-i.py
@@ -0,0 +1,8 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
+        """nums2 = []
+        for i in nums1:
+            if i % 2 == 0:
+                nums2.append(i)
+            return True"""
