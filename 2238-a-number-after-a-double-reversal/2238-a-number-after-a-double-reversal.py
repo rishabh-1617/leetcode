832b571e4a2e1cf0d5aa399d70de6bada10dc30f
Rishabh Prajapati <142728606+rishabh-1617@users.noreply.github.com>
2026-09-08 23:13:36 +0530

    Time: 0 ms (100%), Space: 19.1 MB (85.57%) - LeetHub

---
+++ 2238-a-number-after-a-double-reversal/2238-a-number-after-a-double-reversal.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isSameAfterReversals(self, num: int) -> bool:
+        rev1 = int(str(num)[::-1])
+        rev2 = int(str(rev1)[::-1])
+        if num == rev2:
+            return True
+        return False    
