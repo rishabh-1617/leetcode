f1d6bd7d7d2682605c2ecaa2a57a2b6b9558b069
Rishabh Prajapati <142728606+rishabh-1617@users.noreply.github.com>
2026-09-18 11:30:35 +0530

    Time: 0 ms (100%), Space: 19.2 MB (84.68%) - LeetHub

--- 4051-remove-zeros-in-decimal-representation/4051-remove-zeros-in-decimal-representation.py
+++ 4051-remove-zeros-in-decimal-representation/4051-remove-zeros-in-decimal-representation.py
@@ -2,3 +2,13 @@ class Solution:
     def removeZeros(self, n: int) -> int:
         s = int(str(n).replace('0',''))
         return s
+        """
+        ans = 0
+        place = 1
+        while n:
+            digit = n % 10
+            if digit:
+                ans += digit * place
+                place *= 10
+            n //= 10
+        return ans   """     
